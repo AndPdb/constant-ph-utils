d31c3ecfdad3e5497da285c47aee61c7a0df2809
Andrea Pasquadibisceglie <a.pasquadibisceglie@tigem.it>
2025-09-30 10:24:48 +0200

    Added options for debug and publication

--- run_analysis_template.py
+++ run_analysis_template.py
@@ -10,6 +10,11 @@ from analyses import *
 from plot import *
 import pandas as pd
 
+RUN_TYPE = "Publication" # Debug or Publication
+PLOT_TYPE = "Publication" # Debug or Publication
+NPZ_OUTPUT = True # True or False
+
+# Path variables
 LAMBDAREF_PATH = "test"
 PATH_MD1 = "test/MD1/analysis"
 PATH_MD2 = "test/MD1_2/analysis"
@@ -17,6 +22,7 @@ PATHS_MD = [PATH_MD1, PATH_MD2]
 MD1_PREFIX = "MD1"
 MD2_PREFIX = "MD1_2"
 
+###### Main function #######
 def main():
     lambda_ref = pd.read_csv(os.path.join(LAMBDAREF_PATH ,'lambdareference.dat'), sep=r'\s+', engine='python')
     
@@ -35,58 +41,59 @@ def main():
     # MD1
 
     ## Overview lambda distributions
-    lambda_hist_md1 = plot_lambda_hist(PATH_MD1, xvg_data, coord2lambda_dict, lambda_ref, quality='Publication')
+    lambda_hist_md1 = plot_lambda_hist(PATH_MD1, xvg_data, coord2lambda_dict, lambda_ref, quality=PLOT_TYPE)
     lambda_hist_md1.savefig(f"{MD1_PREFIX}_histograms.png")
     lambda_hist_md1.close()
     
 
     ## Protonation fraction time series
-    proton_ts_md1 = plot_protonation_timeseries(PATH_MD1, time_MD1, xvg_data, coord2lambda_dict, lambda_ref, quality='Publication', npz_output=False)
+    proton_ts_md1 = plot_protonation_timeseries(PATH_MD1, time_MD1, xvg_data, coord2lambda_dict, lambda_ref, quality=PLOT_TYPE, npz_output=NPZ_OUTPUT)
     proton_ts_md1.savefig(f"{MD1_PREFIX}_timeseries.png")
     proton_ts_md1.close()
 
-    # MD2
+    if RUN_TYPE == "Publication":
+        # MD2
 
-    # ## Overview lambda distributions
-    lambda_hist_md2 = plot_lambda_hist(PATH_MD2, xvg_data, coord2lambda_dict, lambda_ref, quality='Publication')
-    lambda_hist_md2.savefig(f"{MD2_PREFIX}_histograms.png")
-    lambda_hist_md2.close()
+        # ## Overview lambda distributions
+        lambda_hist_md2 = plot_lambda_hist(PATH_MD2, xvg_data, coord2lambda_dict, lambda_ref, quality=PLOT_TYPE)
+        lambda_hist_md2.savefig(f"{MD2_PREFIX}_histograms.png")
+        lambda_hist_md2.close()
 
-    # ## Protonation fraction time series
-    proton_ts_md2 = plot_protonation_timeseries(PATH_MD2, time_MD2, xvg_data, coord2lambda_dict, lambda_ref, quality='Publication', npz_output=False)
-    proton_ts_md2.savefig(f"{MD2_PREFIX}_timeseries.png")
-    proton_ts_md2.close()
+        # ## Protonation fraction time series
+        proton_ts_md2 = plot_protonation_timeseries(PATH_MD2, time_MD2, xvg_data, coord2lambda_dict, lambda_ref, quality=PLOT_TYPE, npz_output=NPZ_OUTPUT)
+        proton_ts_md2.savefig(f"{MD2_PREFIX}_timeseries.png")
+        proton_ts_md2.close()
 
-    # MD1 vs MD2
+        # MD1 vs MD2
 
-    # ## Protonation convergence
-    proton_conv = plot_protonation_convergence(PATHS_MD, time_MDs, xvg_data, coord2lambda_dict, lambda_ref, quality='Publication')
-    proton_conv.savefig(f"{MD1_PREFIX}_{MD2_PREFIX}_convergence.png")
-    proton_conv.close()
+        # ## Protonation convergence
+        proton_conv = plot_protonation_convergence(PATHS_MD, time_MDs, xvg_data, coord2lambda_dict, lambda_ref, quality=PLOT_TYPE)
+        proton_conv.savefig(f"{MD1_PREFIX}_{MD2_PREFIX}_convergence.png")
+        proton_conv.close()
 
-    # ## Overview protonation fractions
-    proton_frac = plot_protonation_fraction(PATHS_MD, xvg_data, lambda_ref, npz_output=False)
-    proton_frac.savefig(f"{MD1_PREFIX}_{MD2_PREFIX}_protonfraction.png")
-    proton_frac.close()
+        # ## Overview protonation fractions
+        proton_frac = plot_protonation_fraction(PATHS_MD, xvg_data, lambda_ref, npz_output=NPZ_OUTPUT)
+        proton_frac.savefig(f"{MD1_PREFIX}_{MD2_PREFIX}_protonfraction.png")
+        proton_frac.close()
 
-    # ## Sigle residue protonation fraction time series
-    # ### Glu75
-    glu75_id = resid2coordid(75, lambda_ref)
-    res1_conv = single_residue_convergence(glu75_id, PATHS_MD, xvg_data, lambda_ref)
-    res1_conv.savefig("Glu75.png")
-    res1_conv.close()
-    
-    # ### Glu78
-    glu78_id = resid2coordid(78, lambda_ref)
-    res2_conv = single_residue_convergence(glu78_id, PATHS_MD, xvg_data, lambda_ref)
-    res2_conv.savefig("Glu78.png")
-    res2_conv.close()
+        # ## Sigle residue protonation fraction time series
+        # ### Glu75
+        glu75_id = resid2coordid(75, lambda_ref)
+        res1_conv = single_residue_convergence(glu75_id, PATHS_MD, xvg_data, lambda_ref)
+        res1_conv.savefig("Glu75.png")
+        res1_conv.close()
+        
+        # ### Glu78
+        glu78_id = resid2coordid(78, lambda_ref)
+        res2_conv = single_residue_convergence(glu78_id, PATHS_MD, xvg_data, lambda_ref)
+        res2_conv.savefig("Glu78.png")
+        res2_conv.close()
 
-    # ### Glu513
-    glu513_id = resid2coordid(513, lambda_ref)
-    res3_conv = single_residue_convergence(glu513_id, PATHS_MD, xvg_data, lambda_ref)
-    res3_conv.savefig("Glu513.png")
-    res3_conv.close()
+        # ### Glu513
+        glu513_id = resid2coordid(513, lambda_ref)
+        res3_conv = single_residue_convergence(glu513_id, PATHS_MD, xvg_data, lambda_ref)
+        res3_conv.savefig("Glu513.png")
+        res3_conv.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run analysis script with optional profiling.")
